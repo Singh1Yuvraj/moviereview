d577471e8cec3224c90a36e134f3099f2ed37fcb
Yuvraj Singh <69730200+Singh1Yuvraj@users.noreply.github.com>
2024-03-13 02:00:28 +0530

    integrated add movie api

--- src/AddMoviePopup.js
+++ src/AddMoviePopup.js
@@ -1,8 +1,30 @@
 // AddMoviePopup.js
 import './AddMoviePopup.css';
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
-function AddMoviePopup({ onClose }) {
+function AddMoviePopup({ onClose ,onAddMovie}) {
+  const [movies, setMovies] = useState({name:"", releaseDate:""});
+  
+  function handleInputChange(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    
+    setMovies({ ...movies, [name]: value });
+  }
+  const handleAddMovie = async (e) => {
+    e.preventDefault();
+    // const updatedMovies = [...movies, { ...newMovie, id: Date.now() }];
+    try {
+     let res =await axios.post("http://localhost:5000/add-movie", movies);
+     setMovies({name:"", releaseDate:""})
+    } catch (error) {
+      console.log(error);
+    }
+    // setMovies(updatedMovies);
+    
+  };
   return (
     <div className="popup">
       <div className="popup-inner">
@@ -10,14 +32,14 @@ function AddMoviePopup({ onClose }) {
         <form>
           <div className="form-group">
             <label>Name:</label>
-            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Name "name='name' required onChange={handleInputChange} />
           </div>
           <div className="form-group">
             <label>Release date:</label>
-            <input type="text" placeholder="Release date" />
+            <input type="text" placeholder="Release date" name ='releaseDate'onChange={handleInputChange} />
           </div>
           <div className="button-group">
-            <button type="submit">Create movie</button>
+            <button type="submit" onClick={handleAddMovie}>Create movie</button>
             <button onClick={onClose}>Close</button>
           </div>
         </form>
